75a4659217c4f491d7a5f56cd22eaf2a7296f6f6
Fliegende-Rehe <fliegende-rehe@yandex.ru>
2022-09-13 23:52:18 +0300

    ex1.c

---
+++ week2/ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <limits.h>
+#include <float.h>
+
+void main() {
+	int i = INT_MAX;
+	unsigned short us = USHRT_MAX;
+	long l = LONG_MAX;
+	float f = FLT_MAX;
+	double d = DBL_MAX;
+	
+	printf("%zu	%d\n", sizeof(i), i);
+	printf("%zu	%d\n", sizeof(us), us);
+	printf("%zu	%ld\n", sizeof(l), l);
+	printf("%zu	%f\n", sizeof(f), f);
+	printf("%zu	%f\n", sizeof(d), d);
+}
+
