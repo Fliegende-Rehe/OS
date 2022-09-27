6452dfea5479bd8640a8a1416f920a227e4e4fb2
Fliegende-Rehe <fliegende-rehe@yandex.ru>
2022-09-27 16:03:57 +0300

    setup clion

---
+++ cmake-build-debug/CMakeFiles/F22_OperSys.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F22_OperSys.dir/week4/ex2.c.o"
+  "F22_OperSys"
+  "F22_OperSys.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/F22_OperSys.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
