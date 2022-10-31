0b284e5d7b6a9a874a68168c22848e4eae1d2b43
Fliegende-Rehe <fliegende-rehe@yandex.ru>
2022-11-01 00:21:27 +0300

    asd

--- week8/ex2.c
+++ week8/ex2.c
@@ -3,6 +3,20 @@
 #include <unistd.h>
 #include <time.h>
 
+/* ex2.c
+    If swap is off, the OS kills my program when all free memory is allocated.
+    If I add swap, after all physical memory has been allocated, it uses swap
+    and "si" and "so" fields are greater than 0. But there are often tricks to
+    freeze using swap, since the memory for important operating system programs
+    is copied to swap. And the hard drive is too slow.
+ */
+
+
+/* ex3.c
+    Result displays all the memory of our process
+    and see that the swap memory is too slow.
+ */
+
 int main() {
     time_t begin, end;
     size_t size = 10 * 1024 * 1024;
